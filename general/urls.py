d27e830bb133105681b53d4bc41fe1298856becd
smartwebcoder <smartcoder91@outlook.com>
2019-01-19 19:13:21 +0300

    add swagger ui

--- general/urls.py
+++ general/urls.py
@@ -3,8 +3,10 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_schema_view(title='Service2019 API')
+schema_view = get_swagger_view(title='Pastebin API')
+# schema_view = get_schema_view(title='Service2019 API')
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
